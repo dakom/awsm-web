f6fadbd2fa3d1c656d3c0b1b099b53f7b70e91a1
David Komer <david.komer@gmail.com>
2021-06-10 18:46:04 +0300

    future helper

--- crate/src/loaders/helpers.rs
+++ crate/src/loaders/helpers.rs
@@ -12,6 +12,30 @@ use futures::{
 };
 use gloo_timers::future::TimeoutFuture;
 
+/// Simple way to spawn a future and cancel it by dropping the handle
+pub fn spawn_handle<F, A>(fut: F) -> FutureHandle
+where
+    F: Future<Output = A> + 'static
+{
+    let (fut, handle) = abortable(fut);
+
+    spawn_local(async move {
+        fut.await;
+    });
+
+    FutureHandle { inner: handle }
+}
+
+pub struct FutureHandle {
+    pub inner: AbortHandle 
+}
+
+impl Drop for FutureHandle {
+    fn drop(&mut self) {
+        self.inner.abort();
+    }
+}
+
 /// makes it easier to run futures with a timeout
 ///
 /// future_until(10_000, some_future).await
@@ -24,7 +48,7 @@ use gloo_timers::future::TimeoutFuture;
 ///    some_future3().await;
 /// }).await
 ///
-pub async fn future_until<A, F>(ms: u32, f: F) -> Option<A>
+pub async fn future_until<F, A>(ms: u32, f: F) -> Option<A>
     where F: Future<Output = A> {
 
     pin_mut!(f);
@@ -121,96 +145,3 @@ impl AsyncLoader {
     }
 }
 
-/*
-pub struct AsyncLoader {
-    loading: Mutable<Option<AsyncState>>,
-}
-
-impl Drop for AsyncLoader {
-    fn drop(&mut self) {
-        self.cancel();
-    }
-}
-
-impl AsyncLoader {
-    pub fn new() -> Self {
-        Self {
-            loading: Mutable::new(None),
-        }
-    }
-
-    pub fn cancel(&self) {
-        self.replace(None);
-    }
-
-    fn replace(&self, value: Option<AsyncState>) {
-        let mut loading = self.loading.lock_mut();
-
-        if let Some(state) = loading.as_mut() {
-            state.handle.abort();
-        }
-
-        *loading = value;
-    }
-
-    pub fn load<F>(&self, fut: F) where F: Future<Output = ()> + 'static {
-        let (fut, handle) = abortable(fut);
-
-        let state = AsyncState::new(handle);
-        let id = state.id;
-
-        self.replace(Some(state));
-
-        let loading = self.loading.clone();
-
-        spawn_local(async move {
-            match fut.await {
-                Ok(()) => {
-                    let mut loading = loading.lock_mut();
-
-                    if let Some(current_id) = loading.as_ref().map(|x| x.id) {
-                        // If it hasn't been overwritten with a new state...
-
-                        if current_id == id {
-                            *loading = None;
-                        }
-                    }
-                },
-                // It was already cancelled
-
-                Err(_) => {},
-            }
-        });
-    }
-
-    pub fn is_loading(&self) -> impl Signal<Item = bool> {
-        self.loading.signal_ref(|x| x.is_some())
-    }
-}
-struct AsyncState {
-    id: usize,
-    handle: AbortHandle,
-}
-
-impl AsyncState {
-    fn new(handle: AbortHandle) -> Self {
-        static ID: AtomicUsize = AtomicUsize::new(0);
-
-        let id = ID.fetch_add(1, Ordering::SeqCst);
-
-        Self { id, handle }
-    }
-}
-
-/// Makes it easier to spawn a future into a cancellable signal
-
-#[inline]
-pub fn spawn_future<F>(future: F) -> DiscardOnDrop<CancelableFutureHandle>
-    where F: Future<Output = ()> + 'static {
-    let (handle, future) = cancelable_future(future, || ());
-
-    spawn_local(future);
-
-    handle
-}
-*/
