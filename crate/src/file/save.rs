908976b3efaae5b06ae3b907c3dc64b6f27eba50
David Komer <david.komer@gmail.com>
2023-05-28 13:20:28 +0300

    fix file errors

--- crate/src/file/save.rs
+++ crate/src/file/save.rs
@@ -1,4 +1,7 @@
-pub fn save_file(data: Vec<u8>, filename: &str, mime_type: Option<&str>) {
+use js_sys::Uint8Array;
+use wasm_bindgen::{prelude::*, JsCast};
+
+pub fn save_file(data: Vec<u8>, filename: &str, mime_type: Option<&str>) -> Result<(), JsValue> {
     // cannot use .view() because the WASM memory changes under our feet
     let content = Uint8Array::new_with_length(data.len() as u32);
     content.copy_from(&data);
@@ -10,28 +13,30 @@ pub fn save_file(data: Vec<u8>, filename: &str, mime_type: Option<&str>) {
         Some(mime_type) => {
             let mut blob_options = web_sys::BlobPropertyBag::new();
             blob_options.type_(mime_type);
-            web_sys::Blob::new_with_u8_array_sequence_and_options(&blob_parts, &blob_options).unwrap_ext()
+            web_sys::Blob::new_with_u8_array_sequence_and_options(&blob_parts, &blob_options)?
         },
         None => {
-            web_sys::Blob::new_with_u8_array_sequence(&blob_parts, &blob_options).unwrap_ext()
+            web_sys::Blob::new_with_u8_array_sequence(&blob_parts)?
         }
     };
 
-    let blob_url = web_sys::Url::create_object_url_with_blob(&blob).unwrap_ext();
+    let blob_url = web_sys::Url::create_object_url_with_blob(&blob)?;
 
     let document = web_sys::window().unwrap().document().unwrap();
 
-    let elem:web_sys::HtmlAnchorElement = document.create_element("a").unwrap_ext().unchecked_into();
+    let elem:web_sys::HtmlAnchorElement = document.create_element("a")?.unchecked_into();
 
-    elem.set_attribute("href", &blob_url).unwrap_ext();
-    elem.set_attribute("download", filename).unwrap_ext();
+    elem.set_attribute("href", &blob_url)?;
+    elem.set_attribute("download", filename)?;
 
-    let body = document.body().unwrap_ext();
-    body.append_child(&elem).unwrap_ext();
+    let body = document.body().ok_or_else(|| JsValue::from_str("unable to get document body"))?;
+    body.append_child(&elem)?;
 
     elem.click();
 
-    body.remove_child(&elem).unwrap_ext();
+    body.remove_child(&elem)?;
 
-    web_sys::Url::revoke_object_url(&blob_url).unwrap_ext();
+    web_sys::Url::revoke_object_url(&blob_url)?;
+
+    Ok(())
 }
